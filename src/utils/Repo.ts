a2e5fd722b919cd5a3699036cbdae8210485f7a5
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-02-25 15:05:29 -0300

    module to abstract database operations

---
+++ src/utils/Repo.ts
@@ -0,0 +1,36 @@
+import { AppDataSource } from '../data-source';
+
+const insert = async <T>(entity: any, values: any): Promise<null | T> => {
+  const result = await AppDataSource.manager
+    .createQueryBuilder()
+    .insert()
+    .into(entity)
+    .values(values)
+    .orIgnore() // this is for do nothing when unique constraint violation occours
+    .returning('*')
+    .execute();
+
+  if (Object.keys(result.generatedMaps[0]).length === 0) return null;
+
+  return result.generatedMaps[0] as T;
+};
+
+const update = async (
+  entity: any,
+  id: number,
+  values: any
+): Promise<null | boolean> => {
+  const result = await AppDataSource.manager.update(entity, id, values);
+
+  return !!result.affected;
+};
+
+const findOne = async (entity: any, values: any) => {
+  return await AppDataSource.manager.findOneBy(entity, values);
+};
+
+export const Repo = {
+  insert,
+  update,
+  findOne
+};
